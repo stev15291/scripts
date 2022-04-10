f5e92ba14226f07f3e724dc069987a7036ec8d69
stev15291 <103378210+stev15291@users.noreply.github.com>
2022-04-10 18:54:07 +0200

    Update hatdrop.lua

--- main/hatdrop.lua
+++ main/hatdrop.lua
@@ -2,3 +2,6 @@ local Character = game.Players.LocalPlayer.Character
 Character.HumanoidRootPart:Destroy()
 Character.Torso:Destroy()
 Character.Humanoid.Parent = game.Players.LocalPlayer
+local Fake = Character["Body Colors"]:Clone()
+Character["Body Colors"]:Destroy()
+Fake.Parent = Character
